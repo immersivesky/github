558a887f61f108ff656373bc355c4b622efdfac8
immersivesky <diamondcaspersky@gmail.com>
2023-10-17 05:22:54 +0300

    💚 Fix CI/CD GitHub Actions pipeline

--- pkg/tests/first_test.go
+++ pkg/tests/first_test.go
@@ -3,9 +3,7 @@ package tests
 import "testing"
 
 func TestFirst(t *testing.T) {
-  t.Run("first test", func (t *testing.T) {
-    if true {
-      t.Log("Ok!")
-    }
-  })
+	t.Run("first test", func(t *testing.T) {
+		t.Log("First test finished with status OK!")
+	})
 }
--- pkg/tests/second_test.go
+++ pkg/tests/second_test.go
@@ -3,10 +3,7 @@ package tests
 import "testing"
 
 func TestSecond(t *testing.T) {
-  t.Run("second test", func (t *testing.T) {
-    if false {
-      t.Log("Ok!")
-    }
-    t.Fatal("Err!")
-  })
+	t.Run("second test", func(t *testing.T) {
+		t.Log("Second test finished with status OK")
+	})
 }
